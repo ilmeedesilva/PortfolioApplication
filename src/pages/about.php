5a0bca9751a006c3655c92a19e5986b47db2e916
MadumaliDeAlwis <madumalidealwis@gmail.com>
2023-02-04 17:45:35 +0530

    Changed about page

---
+++ src/pages/about.php
@@ -0,0 +1,98 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/about.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <?php
+    include "../includes/navigation.php";
+    ?>
+    <section class="sec1">
+        <div class="about-1">
+            <h1>
+                ABOUT US
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Minima atque, corporis dolorem magni quibusdam in consequatur doloribus 
+                 quos quo quas deserunt aliquam nesciunt perspiciatis earum neque? Deserunt 
+                 perspiciatis illo et?
+            </p>
+        </div>
+        <div class="about-2">
+            <div class="content_box">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <div class="about-item text-center">
+                                <i class=" fa fa-book">
+                                </i>
+                                <h3>
+                                    BRAND VISION
+                                </h3>
+                                <hr>
+                                <p>
+                                    Lorem ipsum, dolor s
+                                    usantium modi consequuntur dolore ea ex recusandae provident dolorem pariatur,
+                                    s libero cumque, alias inventore aperiam amet? Ratione officia magni doloribus.
+                                </p>
+                            </div>
+
+                        </div>
+                        <div class="col">
+                            <div class="about-item text-center">
+                                <i class="fa fa-globe">
+                                </i>
+                                <h3>
+                                    BRAND TAGLINE
+                                </h3>
+                                <hr>
+                                <p>
+                                    Lorem ipsum, dolor s
+                                    usantium modi consequuntur dolore ea ex recusandae
+                                     provident dolorem pariatur,
+                                    s libero cumque, alias inventore aperiam amet? Ratione
+                                     officia magni doloribus.
+                                </p>
+                            </div>
+
+                        </div>
+                        <div class="col">
+                            <div class="about-item text-center">
+                                <i class="fa fa-pencil">
+                                </i>
+                                <h3>
+                                    MOTTO
+                                </h3>
+                                <hr>
+                                <p>
+                                    Lorem ipsum, dolor s
+                                    usantium modi consequuntur dolore ea ex recusandae
+                                    provident dolorem pariatur,
+                                    s libero cumque, alias inventore aperiam amet? Ra
+                                    tione officia magni doloribus.
+                                </p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+
+    </section>
+    
+</body>
+</html>
+

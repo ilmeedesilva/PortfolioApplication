833ccaf8af6252da1c88282d0415c24280a808d6
difelk <dilshanfernando32@gmail.com>
2023-02-07 17:26:40 +0530

    validate the add products , send and recived data to add product, and show product page db data

---
+++ script/getData.js
@@ -0,0 +1,46 @@
+const getProducts = () => {
+  console.log("get data");
+  fetch("../../db/getProjects.php")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {
+        var project = data[i];
+        var projectName = project.projectName;
+        var date = project.date;
+        var descr = project.descr;
+        var imageEncoded = project.image;
+        var image = '<img src="data:image/jpeg;base64,' + imageEncoded + '"/>';
+        var html =
+          '<div class="col blog_wrapper">\
+                        <div class="blog-top-section">\
+                          <div class="blue_cover"></div>\
+                          <div class="blog_img">' +
+          image +
+          '</div>\
+                        </div>\
+                        <div class="blog-bottom-section">\
+                          <div class="discri">\
+                            <h3>' +
+          projectName +
+          '</h3>\
+                          </div>\
+                          <div class="sub_details d-flex justify-content-between align-items-center">\
+                            <p>' +
+          descr +
+          '</p>\
+                            <div class="date">\
+                              <img src="../../resource/icon/calendar.png" alt="calendar">\
+                              <p>' +
+          date +
+          "</p>\
+                            </div>\
+                          </div>\
+                        </div>\
+                      </div>";
+        document.querySelector(".blog_container").innerHTML += html;
+      }
+    });
+};
+getProducts();

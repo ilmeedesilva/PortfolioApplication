467f8b254dbe75fd9122eadafc0105a12ce96d9b
difelk <dilshanfernando32@gmail.com>
2023-02-20 10:46:10 +0530

    project edit fixed

--- db/putProjectData.php
+++ db/putProjectData.php
@@ -1,1 +1,44 @@
-<php ?>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if (isset($_POST['project_id'])) {
+    $projectId = $_POST['project_id'];
+    $projectName = $_POST['project_name'];
+    $date = $_POST['date'];
+    $descr = $_POST['descr'];
+
+    $sql = "UPDATE projects SET projectName='$projectName', date='$date', descr='$descr' WHERE id=$projectId";
+
+    if (isset($_FILES['image'])) {
+        $file = $_FILES['image'];
+        $fileName = $file['name'];
+        $fileTmpName = $file['tmp_name'];
+        $fileError = $file['error'];
+
+        if ($fileError === 0) {
+            $imageData = base64_encode(file_get_contents($fileTmpName));
+            $sql = "UPDATE projects SET projectName='$projectName', date='$date', descr='$descr', image='$imageData' WHERE id=$projectId";
+        }
+    }
+
+    if ($conn->query($sql) === TRUE) {
+        echo "success";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+} else {
+    echo "No project ID provided";
+}
+
+$conn->close();
+?>

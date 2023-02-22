806b6d312efcfd1e140086926baf82e48eb7939c
MadumaliDeAlwis <madumalidealwis@gmail.com>
2023-02-22 15:30:15 +0530

    update tagline in admin page

--- src/admin/adminAbout.php
+++ src/admin/adminAbout.php
@@ -108,45 +108,49 @@
         </div>
 
 
-
         <div class="collapse_section">
             <div class="collapse_section_header">
                 <h4>Update TagLine</h4>
                 <img src="../../resource/icon/right-arrow.png" alt="arrow" srcset="">
             </div>
             <div class="collapse_section_body">
-                <div class="add_project_wrapper">
-                    <div class="img_upload_container">
-                        <div class="title">
-                            <h5>Upload Image</h5>
+                <div class="project_crud_wrapper">
+                    <p class="project_page_update_pk" style="display: none;"></p>
+                    <div class="selected_cover_img_view selected_img_tagLine_view">
+                    </div>
+                    <div class="project_img_wrapper">
+                        <div class="img_upload_header_txt">
+                            <p>Upload Cover Image</p>
                         </div>
-                        <div class="img_uploader">
-                            <label for="" class="img_uploader_wrapper tagLine_uploader_wrapper">
-                                <input type="file" accept="image/*" class="image_uploader"
-                                    onchange="selectedFileTagLine(this)">
+                        <div class="image_uploader_wrapper img_uploader_label_input_wrapper tagLine_uploader_wrapper">
+                            <label for="image_upload_icon">
+                                <img src="../../resource/icon/uploadIcon.png" alt="Upload Image">
                             </label>
-                            <p class="tagLine_img_name"></p>
-                            <p class="error_txt error_img_tagLine"></p>
-                            <p>Click here to upload</p>
+                            <input type="file" id="tagLine_cover_image-upload" name="cover_image-upload" accept="image/*">
+                            <p>Click the icon to upload image</p>
+                            <p class="error_txt add_tagLine_cover_img_err"></p>
                         </div>
                     </div>
-                    <div class="form_wrapper">
-                        <form action="">
-                            <div class="input_item">
-                                <label for="descr">Description</label><br>
-                                <textarea name="tagLineDesc" cols="30" rows="8"
-                                    placeholder="Type your description here..."></textarea>
-                                <p class="error_txt desc_error_tagLine"></p>
-                            </div>
-                            <div class="form_control">
-                                <button class="save_btn saveTagLine">Save</button>
-                            </div>
-                        </form>
+
+                    <div class="form-container">
+                        <div class="form-group">
+                            <label for="tagLine_page_description">Description</label>
+                            <textarea id="tagLine_page_description" name="tagLine_page_description" rows="8"
+                                cols="30"></textarea>
+                            <p class="error_txt update_tagLine_desc_error"></p>
+                        </div>
+                    </div>
+
+                    <div class="save-container">
+                        <button id="tagLine_page_save" class="primary_btn">Save</button>
                     </div>
                 </div>
             </div>
         </div>
 
+
+
+        
         <div class="collapse_section">
             <div class="collapse_section_header">
                 <h4>Update Motto</h4>

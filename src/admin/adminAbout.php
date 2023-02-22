8de99c0c49b6873876d64b144428b37bfba785da
MadumaliDeAlwis <madumalidealwis@gmail.com>
2023-02-22 14:36:06 +0530

    update about page in admin page

--- src/admin/adminAbout.php
+++ src/admin/adminAbout.php
@@ -29,57 +29,41 @@
 
         <div class="collapse_section">
             <div class="collapse_section_header">
-                <h4>Update about</h4>
+                <h4>Update About</h4>
                 <img src="../../resource/icon/right-arrow.png" alt="arrow" srcset="">
             </div>
             <div class="collapse_section_body">
-
-                <div class="add_project_wrapper">
-
-                    <div class="img_upload_container">
-
-                        <div class="title">
-                            <h5>Upload Image</h5>
+                <div class="project_crud_wrapper">
+                    <p class="project_page_update_pk" style="display: none;"></p>
+                    <div class="selected_cover_img_view selected_img_about_view">
+                    </div>
+                    <div class="project_img_wrapper">
+                        <div class="img_upload_header_txt">
+                            <p>Upload Cover Image</p>
                         </div>
-
-                        <div class="img_uploader">
-
-                            <label for="" class="img_uploader_wrapper about_uploader_wrapper">
-                                <input type="file" accept="image/*" class="image_uploader"
-                                    onchange="selectedFileAbout(this)">
+                        <div class="image_uploader_wrapper img_uploader_label_input_wrapper about_uploader_wrapper">
+                            <label for="image_upload_icon">
+                                <img src="../../resource/icon/uploadIcon.png" alt="Upload Image">
                             </label>
-                            <p class="file_name"></p>
-                            <p class="error_txt error_img_about"></p>
-                            <p>Click here to upload</p>
+                            <input type="file" id="about_cover_image-upload" name="cover_image-upload" accept="image/*">
+                            <p>Click the icon to upload image</p>
+                            <p class="error_txt add_about_cover_img_err"></p>
                         </div>
                     </div>
-                    <div class="form_wrapper">
-                        <form action="">
-                            <div class="input_item">
-                                <label for="descr">Description</label><br>
-                                <textarea name="aboutDesc" cols="30" rows="8"
-                                    placeholder="Type your description here..."></textarea>
-                                <p class="error_txt desc_error_about"></p>
-                            </div>
-                            <div class="form_control">
-                                <button class="save_btn saveAbout">Save</button>
-                            </div>
-                        </form>
+
+                    <div class="form-container">
+                        <div class="form-group">
+                            <label for="about_page_description">Description</label>
+                            <textarea id="about_page_description" name="about_page_description" rows="8"
+                                cols="30"></textarea>
+                            <p class="error_txt update_about_desc_error"></p>
+                        </div>
+                    </div>
+
+                    <div class="save-container">
+                        <button id="about_cover_page_save" class="primary_btn">Save</button>
                     </div>
                 </div>
-                <!-- <div class="update_about">
-                    <div class="desc_wrapper">
-                        <div class="input_item">
-                            <label for="">Description</label><br>
-                            <textarea name="" id="" cols="50" rows="6"
-                                placeholder="Type your description here..."></textarea>
-                            <p class="error_txt"></p>
-                        </div>
-                    </div>
-                    <div class="btn_save_control">
-                        <button class="save_btn primary_btn">Save</button>
-                    </div>
-                </div>  -->
             </div>
         </div>
 

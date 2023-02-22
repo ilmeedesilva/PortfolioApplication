08818d16afd4f68edcf0b0c7752a30c89984d966
MadumaliDeAlwis <madumalidealwis@gmail.com>
2023-02-22 15:06:06 +0530

    update vision data in admin page

--- script/getVision.js
+++ script/getVision.js
@@ -6,10 +6,26 @@ const getVision = () => {
     .then(function (data) {
       for (var i = 0; i < data.length; i++) {
         if (window.location.href.includes("admin")) {
+          if (data[0].image) {
+            document.querySelector(".selected_img_vision_view").style.display =
+              "block";
+            document.querySelector(
+              ".selected_img_vision_view"
+            ).style.backgroundImage = `url('data:image/jpeg;base64,${data[0].image}')`;
+          } else {
+            document.querySelector(".selected_img_vision_view").style.display =
+              "none";
+            console.error("Invalid image data:", data[0].image);
+          }
           document.querySelector(
-            '.form_wrapper textarea[name="visionDesc"]'
+            '.form-container textarea[name="vision_page_description"]'
           ).value = data[0].description;
-        } else {
+
+          document
+            .querySelector("#vision_page_save")
+            .setAttribute("data-pk", data[0].id);
+        } 
+        else {
           var project = data[i];
           var imageEncoded = project.image;
           var image =
@@ -27,3 +43,57 @@ const getVision = () => {
     });
 };
 getVision();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const getVision = () => {
+//   fetch("../../db/getVision.php")
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       for (var i = 0; i < data.length; i++) {
+//         if (window.location.href.includes("admin")) {
+//           document.querySelector(
+//             '.form_wrapper textarea[name="visionDesc"]'
+//           ).value = data[0].description;
+//         } else {
+//           var project = data[i];
+//           var imageEncoded = project.image;
+//           var image =
+//             '<img src="data:image/jpeg;base64,' + imageEncoded + '"/>';
+
+//           document.querySelector(".about_vision .about_vision_img").innerHTML =
+//             image;
+//           document.querySelector(".about_vision_desc p").innerHTML =
+//             data[0].description;
+//         }
+//       }
+//     })
+//     .catch((err) => {
+//       alert(err);
+//     });
+// };
+// getVision();
--- script/updateVision.js
+++ script/updateVision.js
@@ -1,64 +1,155 @@
-const visionInput = document.querySelector(
-  '.vision_uploader_wrapper input[type="file"]'
+const visionPageDescriptionInput = document.getElementById(
+  "vision_page_description"
 );
-const visionClick = document.querySelector(".vision_uploader_wrapper");
+const visionCoverImgInput = document.getElementById("vision_cover_image-upload");
+const selectedVisionCoverImgView = document.querySelector(
+  ".selected_img_vision_view"
+);
+const uploadVisionImageCoverError = document.querySelector(
+  ".add_vision_cover_img_err"
+);
+const visionPageDescErrorMessages = document.querySelector(
+  ".update_vision_desc_error"
+);
+const updateVisionPageSave = document.getElementById("vision_page_save");
 
-const selectedFileVision = (doc) => {
-  const file = doc.files[0];
-  const fileType = file.type;
-  const fileSize = file.size;
-  const filesizeinMB = (file.size / 1000).toFixed(2);
-  document.querySelector(
-    ".vision_img_name"
-  ).innerHTML = `${file.name}, size: ${filesizeinMB} KB`;
-  if (fileType) {
-    document.querySelector(".error_img_vision").innerHTML =
-      "*Please select an image";
-  }
-  if (fileType !== "image/jpeg" && fileType !== "image/png") {
-    document.querySelector(".error_img_vision").innerHTML =
-      "*Invalid file type, only jpeg and png are allowed";
-    return;
-  } else if (fileSize > 1000000) {
-    document.querySelector(".error_img_vision").innerHTML =
-      "*File size must be less than 1MB";
+selectedVisionCoverImgView.style.display = "none";
+
+
+visionCoverImgInput.addEventListener("change", () => {
+  const file = visionCoverImgInput.files[0];
+  const reader = new FileReader();
+
+  // Check if a file was selected
+  if (file) {
+    // Check if the file type is JPG, JPEG, or PNG
+    if (
+      file.type === "image/jpeg" ||
+      file.type === "image/jpg" ||
+      file.type === "image/png"
+    ) {
+      // Check if the file size is less than 1MB
+      if (file.size <= 1048576) {
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+          // Create a new image object and set the onload event handler
+          const img = new Image();
+          img.onload = () => {
+            if (img.width > 100) {
+              selectedVisionCoverImgView.style.backgroundImage = `url('${reader.result}')`;
+              selectedVisionCoverImgView.style.display = "block";
+              uploadVisionImageCoverError.textContent = "";
+            } else {
+              uploadVisionImageCoverError.textContent =
+                "Please select an image with a width greater than 100px.";
+                selectedVisionCoverImgView.style.display = "none";
+            }
+          };
+          img.src = reader.result;
+        };
+      } else {
+        uploadVisionImageCoverError.textContent =
+          "Please select an image with a file size less than 1MB.";
+        selectedVisionCoverImgView.style.display = "none";
+      }
+    } else {
+      uploadVisionImageCoverError.textContent =
+        "Please select an image of type JPG, JPEG, or PNG.";
+      selectedVisionCoverImgView.style.display = "none";
+    }
   } else {
-    document.querySelector(".error_img_vision").innerHTML = "";
+    selectedVisionCoverImgView.style.display = "none";
   }
-};
-
-visionClick.addEventListener("click", (e) => {
-  visionInput.click();
 });
 
-const descVision = document.querySelector('textarea[name="visionDesc"]');
+updateVisionPageSave.addEventListener("click", () => {
+  const vision_update_id = updateVisionPageSave.getAttribute("data-pk");
+  // errorMessages.forEach((errorMessage) => {
+  //   errorMessage.textContent = "";
+  // });
 
-const submit_button_vision = document.querySelector(".saveVision");
-
-const err_desc_vision = document.querySelector(".desc_error_vision");
-
-document.querySelector(".saveVision").addEventListener("click", (e) => {
-  e.preventDefault();
-  const err_img_vision = document.querySelector(".error_img_vision");
-  if (!visionInput.value) {
-    err_img_vision.innerHTML = "*Please select an image";
+  const description = visionPageDescriptionInput.value;
+  const image = visionCoverImgInput.files[0];
+  let isInvalidIMG = false;
+  if (!image && !vision_update_id) {
+    uploadVisionImageCoverError.textContent = "*Please select an image";
+  } else if (
+    image &&
+    !image.type.includes("jpeg") &&
+    !image.type.includes("png")
+  ) {
+    uploadVisionImageCoverError.textContent = "*Please select a JPEG or PNG image";
+    isInvalidIMG = true;
+  } else if (image && image.size > 1000000) {
+    uploadVisionImageCoverError.textContent = "*The image must be less than 1MB";
+    isInvalidIMG = true;
   } else {
-    err_img_vision.innerHTML = "";
+    uploadVisionImageCoverError.textContent = "";
   }
-  if (!descVision.value) {
-    err_desc_vision.innerHTML = "*Description cannot be empty";
-  } else if (descVision.value.length <= 99 || descVision.value.length > 500) {
-    err_desc_vision.innerHTML =
-      "*Vision Description should contain more than 100 characters and less than 500 characters";}  
-  else {
-    err_desc_vision.innerHTML = "";
+
+  if (description.trim() === "") {
+    visionPageDescErrorMessages.textContent = "*Please enter a description";
+  } else if (description.length <= 199 || description.length > 1000) {
+    visionPageDescErrorMessages.textContent =
+      "*The description must be at least 200 characters long";
+  } else {
+    visionPageDescErrorMessages.textContent = "";
   }
-  if (!err_desc_vision.innerHTML && !err_img_vision.innerHTML) {
-    document.querySelector(".error_img_vision").innerHTML = "";
-    const selectedFileAbout2 = visionInput.files[0];
+
+  if (vision_update_id && description && visionPageDescErrorMessages.textContent=="" && !isInvalidIMG) {
+    if (image) {
+      const formData = new FormData();
+      formData.append("description", description);
+      formData.append("image", image);
+
+      fetch("../../db/updateVision.php", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.text())
+        .then((result) => {
+          console.log("result = ", result);
+          displayPopUp(result);
+          // if (result.includes("success")) {
+
+          // } else {
+          //   alert("Failed to save data");
+          // }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    } else {
+      const formData = new FormData();
+      formData.append("description", description);
+      formData.append("id", parseInt(vision_update_id));
+      fetch("../../db/updateVisionPageById.php", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.text())
+        .then((result) => {
+          displayPopUp(result);
+          // if (result.includes("success")) {
+
+          // } else {
+          //   alert("Failed to save data");
+          // }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+  }
+
+  if (
+    !vision_update_id &&
+    !uploadVisionImageCoverError.textContent &&
+    !visionPageDescErrorMessages.textContent
+  ) {
     const formData = new FormData();
-    formData.append("descVision", descVision.value);
-    formData.append("image", selectedFileAbout2);
+    formData.append("description", description);
+    formData.append("image", image);
 
     fetch("../../db/updateVision.php", {
       method: "POST",
@@ -67,8 +158,6 @@ document.querySelector(".saveVision").addEventListener("click", (e) => {
       .then((response) => response.text())
       .then((result) => {
         displayPopUp(result);
-
-        // displayPopUp(result);
         // if (result.includes("success")) {
 
         // } else {
@@ -76,15 +165,121 @@ document.querySelector(".saveVision").addEventListener("click", (e) => {
         // }
       })
       .catch((error) => {
-        alert(error);
+        console.error("Error:", error);
       });
   }
-});
+})
 
-// document.querySelector(".clearVision").addEventListener("click", (e) => {
-//   e.preventDefault();
 
-//   descVision.value = "";
-//   visionInput.value = "";
-//   document.querySelector(".vision_img_name").innerHTML = "";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const visionInput = document.querySelector(
+//   '.vision_uploader_wrapper input[type="file"]'
+// );
+// const visionClick = document.querySelector(".vision_uploader_wrapper");
+
+// const selectedFileVision = (doc) => {
+//   const file = doc.files[0];
+//   const fileType = file.type;
+//   const fileSize = file.size;
+//   const filesizeinMB = (file.size / 1000).toFixed(2);
+//   document.querySelector(
+//     ".vision_img_name"
+//   ).innerHTML = `${file.name}, size: ${filesizeinMB} KB`;
+//   if (fileType) {
+//     document.querySelector(".error_img_vision").innerHTML =
+//       "*Please select an image";
+//   }
+//   if (fileType !== "image/jpeg" && fileType !== "image/png") {
+//     document.querySelector(".error_img_vision").innerHTML =
+//       "*Invalid file type, only jpeg and png are allowed";
+//     return;
+//   } else if (fileSize > 1000000) {
+//     document.querySelector(".error_img_vision").innerHTML =
+//       "*File size must be less than 1MB";
+//   } else {
+//     document.querySelector(".error_img_vision").innerHTML = "";
+//   }
+// };
+
+// visionClick.addEventListener("click", (e) => {
+//   visionInput.click();
 // });
+
+// const descVision = document.querySelector('textarea[name="visionDesc"]');
+
+// const submit_button_vision = document.querySelector(".saveVision");
+
+// const err_desc_vision = document.querySelector(".desc_error_vision");
+
+// document.querySelector(".saveVision").addEventListener("click", (e) => {
+//   e.preventDefault();
+//   const err_img_vision = document.querySelector(".error_img_vision");
+//   if (!visionInput.value) {
+//     err_img_vision.innerHTML = "*Please select an image";
+//   } else {
+//     err_img_vision.innerHTML = "";
+//   }
+//   if (!descVision.value) {
+//     err_desc_vision.innerHTML = "*Description cannot be empty";
+//   } else if (descVision.value.length <= 99 || descVision.value.length > 500) {
+//     err_desc_vision.innerHTML =
+//       "*Vision Description should contain more than 100 characters and less than 500 characters";}  
+//   else {
+//     err_desc_vision.innerHTML = "";
+//   }
+//   if (!err_desc_vision.innerHTML && !err_img_vision.innerHTML) {
+//     document.querySelector(".error_img_vision").innerHTML = "";
+//     const selectedFileAbout2 = visionInput.files[0];
+//     const formData = new FormData();
+//     formData.append("descVision", descVision.value);
+//     formData.append("image", selectedFileAbout2);
+
+//     fetch("../../db/updateVision.php", {
+//       method: "POST",
+//       body: formData,
+//     })
+//       .then((response) => response.text())
+//       .then((result) => {
+//         displayPopUp(result);
+
+//         // displayPopUp(result);
+//         // if (result.includes("success")) {
+
+//         // } else {
+//         //   alert("Failed to save data");
+//         // }
+//       })
+//       .catch((error) => {
+//         alert(error);
+//       });
+//   }
+// });
+
+// // document.querySelector(".clearVision").addEventListener("click", (e) => {
+// //   e.preventDefault();
+
+// //   descVision.value = "";
+// //   visionInput.value = "";
+// //   document.querySelector(".vision_img_name").innerHTML = "";
+// // });

84fedd3e785f5d433468e83681c6cecced59811e
difelk <dilshanfernando32@gmail.com>
2023-03-11 16:34:54 +0530

    home cover

--- script/getHomeCover.js
+++ script/getHomeCover.js
@@ -1,45 +1,48 @@
 const getHomeCover = () => {
-    isLoading(true);
-    fetch("../../db/getHomeCover.php")
-      .then(function (response) {
-        isLoading(false);
-        return response.json();
-      })
-      .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-          if (window.location.href.includes("admin")) {
-            if (data[0].image) {
-              document.querySelector(".selected_home_cover_img_view").style.display =
-                "block";
-              document.querySelector(
-                ".selected_home_cover_img_view"
-              ).style.backgroundImage = `url('data:image/jpeg;base64,${data[0].image}')`;
-            } else  {
-              document.querySelector(".selected_home_cover_img_view").style.display =
-                "none";
-              console.error("Invalid image data:", data[0].image);
-            }
-            document.getElementById("header-text").value = data[0].headerText;
-            document.getElementById("cover-description").value = data[0].description;
-  
-            document
-              .querySelector(".home_cover_save_btn")
-              .setAttribute("data-pk", data[0].id);
-          } else{
-            console.log("else");
+  isLoading(true);
+  fetch("../../db/getHomeCover.php")
+    .then(function (response) {
+      isLoading(false);
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.length; i++) {
+        if (window.location.href.includes("admin")) {
+          if (data[0].image) {
             document.querySelector(
-              ".main_banner"
+              ".selected_home_cover_img_view"
+            ).style.display = "block";
+            document.querySelector(
+              ".selected_home_cover_img_view"
             ).style.backgroundImage = `url('data:image/jpeg;base64,${data[0].image}')`;
-            document.querySelector(".banner_text_section h2").innerHTML =
-              data[0].headerText;
-            document.querySelector(".banner_text_section p").innerHTML = data[0].description;
+          } else {
+            document.querySelector(
+              ".selected_home_cover_img_view"
+            ).style.display = "none";
+            console.error("Invalid image data:", data[0].image);
           }
+          document.getElementById("header-text").value = data[0].headerText;
+          document.getElementById("cover-description").value =
+            data[0].description;
+
+          document
+            .querySelector(".home_cover_save_btn")
+            .setAttribute("data-pk", data[0].id);
+        } else {
+          console.log("else");
+          document.querySelector(
+            ".main_banner"
+          ).style.backgroundImage = `url('data:image/jpeg;base64,${data[0].image}')`;
+          document.querySelector(".banner_text_section h2").innerHTML =
+            data[0].headerText;
+          document.querySelector(".banner_text_section p").innerHTML =
+            data[0].description;
         }
-      })
-      .catch((err) => {
-        console.log(err);
-        // alert(err);
-      });
-  };
-  getHomeCover();
-  console.log("else2");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      // alert(err);
+    });
+};
+getHomeCover();
